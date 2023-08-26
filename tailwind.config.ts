ae98b882a9e690d2db1ef546fcd3fcc02129eca4
Leonardo Grgincic <grgincic.pri>
2023-08-26 19:41:23 +0200

    sdada

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      // main: "#070B16",
+      dark: "#181846",
+      orange: "#FF5722",
+      whisper: "#ECEDF5",
+      ghost: "#C4C5D7",
+      ebony: "#0C0D25",
+      lynch: "#5F6B8A",
+    },
+    fontSize: {
+      xs: ["12px", { lineHeight: "20px", letterSpacing: "0.03em" }],
+      base: ["16px", { lineHeight: "32px" }],
+      md: ["25.89px", { lineHeight: "41.1px" }],
+      lg: ["41.89px", { lineHeight: "68px" }],
+      xl: ["67.77px", { lineHeight: "81.3px" }],
+    },
+    container: {
+      padding: "20px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
